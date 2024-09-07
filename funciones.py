2a0bbb3d6de96fb1b6a080283fb3eda8c668eeac
TrusVz <rondonluis982@gmail.com>
2024-09-07 09:50:49 -0400

    First commit

---
+++ funciones.py
@@ -0,0 +1,27 @@
+# def NombreDeFuncion(parametros):
+
+nombre = "Luis"
+numero1 = 2
+numero2 = 3
+
+def Saludo ():
+    print ("Hola mucho gusto " + nombre)
+
+def Sumar ():
+        num1 = 10
+        num2 = 20
+        # print(num1 + num2)
+        suma = num1 + num2 
+        return suma
+
+def Resta (numero1, numero2):
+      
+    resta = numero1 - numero2
+    return resta 
+
+print (Resta(numero1, numero2) + 10)
+
+# print (Sumar () + 10)
+
+Sumar ()
+# Saludo()
---
+++ while.py
@@ -0,0 +1,9 @@
+# While (condicion):
+    # Lo que este dentro del while
+    # se ejecuta hasta que la condicion sea falsa 
+
+edad = 10
+
+while edad < 10:
+    print("Hola")
+    
